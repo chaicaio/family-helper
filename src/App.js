6e1cbfaa80901fd10dbd4534e3a479ce234d3dd6
artemtk <art@bytefoods.co>
2021-08-23 17:08:29 -0700

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsConfig from './aws-exports';
+import {AmplifyAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
+
+Amplify.configure(awsConfig);
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Amplify</h1>
-    </div>
+    <AmplifyAuthenticator>
+      <div className="App">
+        <h1>Welcome to Amplify</h1>
+        <AmplifySignOut />
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
 export default App;
- 
